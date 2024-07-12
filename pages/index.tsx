ff5d124ac78ac415d9bd118b4a5dcac120d7365e
big7983 <79016020+big7983@users.noreply.github.com>
2024-07-12 16:04:51 +0700

    12/07 backup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layouts/Layout";
+import { userSelector } from "@/store/slices/userSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+type Props = {};
+
+export default function Index({}: Props) {
+  
+  const user = useSelector(userSelector);
+
+  return (
+    <Layout>
+      <div>index {user.username}</div>
+    </Layout>
+  );
+}
